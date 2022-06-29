f6842bb77915269097611cb21a9bf52a028590e8
Arthur Madureira <arthurmadureira10@gmail.com>
2022-06-29 18:17:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Advice Generator
+
+## This is a project that generates advices
+
+### Because sometimes we need advice
+
+### Techs:
+
+- HTML
+- CSS
+- JS
+- Adivice Slip Api(https://api.adviceslip.com)
+
+# Enjoy 👋
---
+++ main.js
@@ -0,0 +1,14 @@
+const url = 'https://api.adviceslip.com/advice'
+
+function getAdvice() {
+  axios.get(url)
+  .then(res => {
+    advice.textContent = res.data.slip.advice
+    adviceId.textContent = `Advice #${res.data.slip.id}`
+  })
+  .catch(error => console.log(error))
+}
+
+const diceBtn = document.getElementById('diceBtn')
+
+diceBtn.addEventListener('click', getAdvice)
